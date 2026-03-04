fcb94decc6f5e658f871113d94ffeb7173627380
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-04 22:28:38 +0200

    refined db.py and added comments

--- backend/app/db.py
+++ backend/app/db.py
@@ -1,6 +1,12 @@
-import os
-import psycopg2
+import os # module for communication with operating system
+import psycopg2 # driver for interaction with PostgreSQL database
 
+'''
+1) Establish connection to the database
+2) Take environmental variables from .env file located on the server and use 
+these values as credentials
+3) Wrap the connection parameters into GET_CONNECTION function for reiteration
+'''
 def get_connection():
 	return psycopg2.connect(
 		host=os.getenv("DB_HOST"),
@@ -10,19 +16,21 @@ def get_connection():
 		password=os.getenv("DB_PASS"),
 	)
 
+# Read data from the database and return values as an outcome
+# Added 'finally' section to avoid uncatched errors or wrong queries
 def fetch_all(query, params=None):
-	conn = get_connection()
-	cur = conn.cursor()
-	cur.execute(query, params or ())
-	rows = cur.fetchall()
-	cur.close()
-	conn.close()
-	return rows
+    conn = get_connection()
+    try:
+        cur = conn.cursor()
+        cur.execute(query, params or ())
+        return cur.fetchall()
+    finally:
+        cur.close()
+        conn.close()
 
+# Execute write-query and apply changes
 def execute(query, params=None):
-	conn = get_connection()
-	cur = conn.cursor()
-	cur.execute(query, params or ())
-	conn.commit()
-	cur.close()
-	conn.close()
+	with get_connection() as conn:
+		with conn.cursor() as cur:
+			cur.execute(query, params or ())
+			conn.commit()

127ef217090cc2bf5b071a741f409d1e36d8f602
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-04 23:16:06 +0200

    Refined main.py and added TBA for further development of monitoring system

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,21 @@
-from fastapi import FastAPI
+# Import main class for web application creation
+from fastapi import FastAPI 
+# Import router from app/api/releases.py 
 from app.api.releases import router as releases_router
 
+# Create an app-object
 app = FastAPI()
-
+# Connect earlier added router to get releases
 app.include_router(releases_router, prefix="/api")
 
+# Root address. Useful for health and availability checks (monitoring)
 @app.get("/")
 def root():
 	return {"status": "ok", "message": "Metal Intel backend is running"}
+
+'''
+TBA:
+--- Lifespan (Startup / Shutdown) - check connection with the database on startup
+--- Middleware (CORS) - in case of frontend on another port / domain in future
+--- Error handler - correct error handler for structured logging
+'''

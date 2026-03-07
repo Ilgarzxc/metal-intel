6237b8c5f652e4a4af6e66fa8cfc2f15c938dcad
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-08 00:10:20 +0200

    test

--- backend/etl/fetch_releases.py
+++ backend/etl/fetch_releases.py
@@ -7,7 +7,7 @@ logging.basicConfig(
     level=logging.INFO,
     format='%(asctime)s  - %(levelname)s - %(message)s',
     filename='fetcher.log'
-    )
+)
 logger = logging.getLogger(__name__)
 
 # Белый список жанров

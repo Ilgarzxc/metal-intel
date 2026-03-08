69301e0bf8e3cbafa91a3f71fdd95da59ba7979b
Ilgar Gurbanov <ilgar.gurbanov.90@gmail.com>
2026-03-08 14:45:20 +0200

    refined releases.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,21 +1,11 @@
-# Import main class for web application creation
-from fastapi import FastAPI 
-# Import router from app/api/releases.py 
-from app.api.releases import router as releases_router
+import asyncio
+from fastapi import FastAPI
+from app.api import releases
+from app.db import init_pool
 
-# Create an app-object
 app = FastAPI()
-# Connect earlier added router to get releases
-app.include_router(releases_router, prefix="/api")
+app.include_router(releases.router, prefix="/api")
 
-# Root address. Useful for health and availability checks (monitoring)
-@app.get("/")
-def root():
-	return {"status": "ok", "message": "Metal Intel backend is running"}
-
-'''
-TBA:
---- Lifespan (Startup / Shutdown) - check connection with the database on startup
---- Middleware (CORS) - in case of frontend on another port / domain in future
---- Error handler - correct error handler for structured logging
-'''
+@app.on_event("startup")
+async def startup():
+    await init_pool()
